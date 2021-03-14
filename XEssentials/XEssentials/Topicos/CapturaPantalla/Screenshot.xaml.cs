6b3935eaa6524f56e0ae7a9927e5273808e277a2
Eleazar Saavedra <eleazarsaavedra@gmail.com>
2021-03-14 12:20:24 -0430

    Agregar archivos de proyecto.

---
+++ XEssentials/XEssentials/Topicos/CapturaPantalla/Screenshot.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace XEssentials.Topicos.CapturaPantalla
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Screenshot : ContentPage
+    {
+        public Screenshot()
+        {
+            InitializeComponent();
+        }
+
+        async void Button_Clicked(object sender, EventArgs e)
+        {
+            var screenshot = await Xamarin.Essentials.Screenshot.CaptureAsync();
+            var stream = await screenshot.OpenReadAsync();
+
+            ImageScreenshot.Source = ImageSource.FromStream(() => stream);
+        }
+    }
+}
---
+++ XEssentials/XEssentials/Topicos/FilePicker/FilePicker.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace XEssentials.Topicos.FilePicker
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class FilePicker : ContentPage
+    {
+        public FilePicker()
+        {
+            InitializeComponent();
+        }
+
+        async void Button_Clicked(object sender, EventArgs e)
+        {
+            try
+            {
+                // Also we can select the type of file tha we allow.
+                // See the documentation.
+                //var file2 = await Xamarin.Essentials.FilePicker.PickAsync(
+                //    new Xamarin.Essentials.PickOptions
+                //    {
+                //        FileTypes = new Xamarin.Essentials.FilePickerFileType()
+                //    }); ;
+
+                var file = await Xamarin.Essentials.FilePicker.PickAsync();
+                if (file == null)
+                    return;
+
+                LabelFile.Text = file.FileName;
+            }
+            catch (Exception exc)
+            {
+                
+            }
+        }
+    }
+}
